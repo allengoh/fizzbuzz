0ca4dec37a62962738f6ceb7159acf3779888851
allengoh <allen1311@hotmail.com>
2022-07-16 11:33:02 +0100

    Added "buzz" and "fizzbuzz" rspec

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,19 @@
 require 'fizzbuzz'
 
+describe "fizzbuzz" do
+  it 'returns "fizzbuzz" when passed both multiples of 3 and 5' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
+  end
+end
+
 describe "fizzbuzz" do 
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed multiples of 3' do
     expect(fizzbuzz(3)).to eq "fizz"
+  end
+end
+
+describe "fizzbuzz" do
+  it 'returns "buzz" when passed multiples of 5' do
+    expect (fizzbuzz(5)).to eq "buzz"
   end
 end
